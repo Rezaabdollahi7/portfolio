9251e5ada1d2560780df990b1ff3d34672a100d2
Rezaabdollahi7 <srezaabdollahi7@gmail.com>
2023-09-22 14:02:31 +0330

    add items to skiils & reset commit

--- src/App.js
+++ src/App.js
@@ -1,16 +1,16 @@
 // import components
-import Navbar from "./components/navbar/navbar"
-import AboutMe from "./components/aboutMe/aboutMe";
-import BestServices from "./components/bestServices/bestServices";
-import Education from "./components/Education/Education";
-import Work_skill from "./components/work_skill/work_skill";
-import Funfact from "./components/Funfact/Funfact";
-import FreelancerWork from "./components/FreelancerWork/FreelancerWork";
-import PricingPlans from "./components/PricingPlans/PricingPlans";
-import AmazingWork from "./components/AmazingWork/AmazingWork";
-import FeaturedPosts from "./components/Featured posts/FeaturedPosts";
-import ContactMe from "./components/ContactMe/ContactMe";
-import CustomFooter from "./components/Footer/CustomFooter";
+import Navbar from "./components/Main Page/navbar/navbar"
+import AboutMe from "./components/Main Page/aboutMe/aboutMe";
+import BestServices from "./components/Main Page/bestServices/bestServices";
+import Education from "./components/Main Page/Education/Education";
+import Work_skill from "./components/Main Page/work_skill/work_skill";
+import Funfact from "./components/Main Page/Funfact/Funfact";
+import FreelancerWork from "./components/Main Page/FreelancerWork/FreelancerWork";
+import PricingPlans from "./components/Main Page/PricingPlans/PricingPlans";
+import AmazingWork from "./components/Main Page/AmazingWork/AmazingWork";
+import FeaturedPosts from "./components/Main Page/Featured posts/FeaturedPosts";
+import ContactMe from "./components/Main Page/ContactMe/ContactMe";
+import CustomFooter from "./components/Main Page/Footer/CustomFooter";
 
 function App() {
   return (
