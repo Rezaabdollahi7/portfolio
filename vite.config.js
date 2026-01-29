f90e2a1225aa33cad292c8b49303c383e8cc7964
Rezaabdollahi7 <srezaabdollahi7@gmail.com>
2026-01-29 14:32:21 +0330

    ✨ feat: update favicon and title in index.html

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,24 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
+import { resolve, dirname } from "path";
+import { fileURLToPath } from "url";
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(),tailwindcss()],
-})
+  plugins: [react(), tailwindcss()],
+  resolve: {
+    alias: {
+      "#components": resolve(
+        dirname(fileURLToPath(import.meta.url)),
+        "components"
+      ),
+      "#constants": resolve(
+        dirname(fileURLToPath(import.meta.url)),
+        "constants"
+      ),
+      "#store": resolve(dirname(fileURLToPath(import.meta.url)), "store"),
+      "#hoc": resolve(dirname(fileURLToPath(import.meta.url)), "hoc"),
+      "#windows": resolve(dirname(fileURLToPath(import.meta.url)), "windows"),
+    },
+  },
+});
