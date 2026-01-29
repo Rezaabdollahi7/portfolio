dcfefd9df91d6f6c424fb2396dbf083bb1ea52c7
Rezaabdollahi7 <srezaabdollahi7@gmail.com>
2026-01-30 00:43:08 +0330

    📝 docs: add Welcome component and update imports in App

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,9 @@
-import NavBar from "#components/Navbar.jsx";
-
+import { NavBar, Welcome } from "#components";
 const App = () => {
   return (
     <main>
       <NavBar />
+      <Welcome />
     </main>
   );
 };
---
+++ src/components/index.js
@@ -0,0 +1,4 @@
+import NavBar from "./Navbar";
+import Welcome from "./Welcome";
+
+export { NavBar, Welcome };
